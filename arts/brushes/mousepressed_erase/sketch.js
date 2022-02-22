117c8816e71832920c7d2e40ac98acecce123dc6
jdeboi <jdeboi@gmail.com>
2022-02-22 10:00:39 -0800

    delete brush

--- arts/brushes/mousepressed_erase/sketch.js
+++ arts/brushes/mousepressed_erase/sketch.js
@@ -7,15 +7,13 @@ function setup() {
 function draw() {
 
   if (mouseIsPressed) {
-    console.log("PRESSED " + frameCount);
     noStroke()
     fill(random(255), random(255), random(255));
     ellipse(mouseX, mouseY, random(5, 50));
 
   }
 
-  if (keyIsDown(32)) {
-    console.log("SPACE PRESSED " + frameCount);
+  if (keyIsDown(8)) {
     fill(255);
     ellipse(mouseX, mouseY, 50);
   }
