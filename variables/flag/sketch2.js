f9f9be656ff424254e29a2e89a39bfe1ac56fdf2
jdeboi <jdeboi@gmail.com>
2022-09-11 13:51:04 -0700

    flag mouse

--- variables/flag/sketch2.js
+++ variables/flag/sketch2.js
@@ -1,5 +1,5 @@
 let w = 200;
-let h = w*2/3;
+let h = w * 2 / 3;
 let x = 100;
 let y = 100;
 
@@ -9,17 +9,21 @@ function setup() {
 
 function draw() {
   background(255);
-let w = mouseX-x;
-let h = w*2/3;
+  let w = mouseX - x;
+  let h = w * 2 / 3;
+
+  if (mouseIsPressed) {
+    h = mouseY - y;
+  }
   // flag code
   fill('blue');
-  rect(x, y, w/3, h);
+  rect(x, y, w / 3, h);
   fill('white');
-  rect(x + w/3,y , w/3, h);
+  rect(x + w / 3, y, w / 3, h);
   fill('red');
-  rect(x + 2*w/3,y , w/3, h);
+  rect(x + 2 * w / 3, y, w / 3, h);
 
-  
+
   // x++;
   // y++;
 
