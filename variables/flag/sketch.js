2ba3bba84932ea90dbeeb61053c5c765470e0f13
jdeboi <jdeboi@gmail.com>
2022-09-11 13:39:41 -0700

    flag

---
+++ variables/flag/sketch.js
@@ -0,0 +1,26 @@
+let w = 200;
+let h = w*2/3;
+let x = 100;
+let y = 100;
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255);
+let w = mouseX-x;
+let h = w*2/3;
+  // flag code
+  fill('blue');
+  rect(x, y, w/3, h);
+  fill('white');
+  rect(x + w/3,y , w/3, h);
+  fill('red');
+  rect(x + 2*w/3,y , w/3, h);
+
+  
+  // x++;
+  // y++;
+
+}
