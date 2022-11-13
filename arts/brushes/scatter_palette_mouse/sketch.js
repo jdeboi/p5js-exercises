3f890790af42d4b5a8af8d1052d5589c5871cb85
jdeboi <jdeboi@gmail.com>
2022-11-13 14:34:21 -0800

    brush

---
+++ arts/brushes/scatter_palette_mouse/sketch.js
@@ -0,0 +1,28 @@
+
+function setup() {
+  createCanvas(500, 500);
+  background(255);
+}
+
+function draw() {
+  let palette = ['red', 'turquoise', 'blue', 'purple']; 
+
+  if (keyIsDown(8)) {
+    fill(255);
+    ellipse(mouseX, mouseY, 50);
+  }
+  else {
+    if (mouseIsPressed) {
+      fill(random(255), random(255), random(255));
+      // background(0, 0, 255, 5);
+      noStroke();
+      // fill(255);
+      ellipse(mouseX, mouseY, random(5, 50));
+    
+      // fill(255);
+      ellipse(mouseX+random(-50, 50), mouseY+random(-50, 50), 5);
+    }
+  }
+  
+  
+}
